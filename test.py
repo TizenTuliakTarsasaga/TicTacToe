57fddd2b47577194a8bfd29caefced715548d51d
gaborKriger <regirck@gmail.com>
2018-07-04 14:32:38 +0200

    Colour

--- test.py
+++ test.py
@@ -20,6 +20,8 @@ def start():
             button['text'] = "O"
             click = True
 
+        button1.configure(bg = "red", fg = "red")
+
         if (button1["text"] == "X" and button2["text"] == "X" and button3["text"] == "X" or
             button4["text"] == "X" and button5["text"] == "X" and button6["text"] == "X" or
             button7["text"] == "X" and button8["text"] == "X" and button9["text"] == "X" or
